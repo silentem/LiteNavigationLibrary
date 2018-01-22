23438f0cb847cc1649cf34fd7c2467bd515e6364
Artem Kovalenko <silentem1113@gmail.com>
2018-01-23 01:51:52 +0200

    Initial commit

---
+++ app/src/main/java/com/whaletail/lite/litenavigationlibrary/MainActivityExample.java
@@ -0,0 +1,13 @@
+package com.whaletail.lite.litenavigationlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_example);
+    }
+}
