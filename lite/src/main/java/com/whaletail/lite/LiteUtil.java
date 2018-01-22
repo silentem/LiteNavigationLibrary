b72c06beba1f5cd91d353de526ad24eea8147879
Artem Kovalenko <silentem1113@gmail.com>
2018-01-23 01:54:22 +0200

    create test method

---
+++ lite/src/main/java/com/whaletail/lite/LiteUtil.java
@@ -0,0 +1,14 @@
+package com.whaletail.lite;
+
+/**
+ * @author whaletail
+ * @date 23.01.18.
+ */
+
+public class LiteUtil {
+
+    public static String sayHello() {
+        return "Hello";
+    }
+
+}
